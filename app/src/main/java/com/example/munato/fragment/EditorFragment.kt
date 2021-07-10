be29b8db25f4f149583f0962b66a90429cbe2c71
kmortyk <casha.dryaba@mail.ru>
2021-07-10 12:33:04 +0300

    add butt

---
+++ app/src/main/java/com/example/munato/fragment/EditorFragment.kt
@@ -0,0 +1,51 @@
+package com.example.munato.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.munato.R
+
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+// private const val ARG_PARAM1 = "param1"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [EditorFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class EditorFragment : Fragment() {
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         * @return A new instance of fragment EditorFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            EditorFragment().apply {
+                arguments = Bundle().apply {
+                    // putString(ARG_PARAM1, param1)
+                }
+            }
+    }
+
+    // private var param1: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            // param1 = it.getString(ARG_PARAM1)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_editor, container, false)
+    }
+}
