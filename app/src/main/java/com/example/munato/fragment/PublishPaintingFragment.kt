1bb015389bf54cac43d8cd5caa6c05bc474545a0
kmortyk <casha.dryaba@mail.ru>
2021-07-16 02:57:20 +0300

    add copy nehaviour

--- app/src/main/java/com/example/munato/fragment/PublishPaintingFragment.kt
+++ app/src/main/java/com/example/munato/fragment/PublishPaintingFragment.kt
@@ -1,15 +1,24 @@
 package com.example.munato.fragment
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Context.CLIPBOARD_SERVICE
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.example.munato.R
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.snackbar.Snackbar
 import java.security.SecureRandom
+
 
 private const val ARG_CODE = "code"
 const val PAINTING_ID_SIZE = 256
@@ -54,6 +63,8 @@ class PublishPaintingFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_publish_painting, container, false)
         val tvPaintingID = view.findViewById<TextView>(R.id.tv_painting_id)
         val cardViewPaintingIDBar = view.findViewById<CardView>(R.id.card_view_painting_id_bar)
+        val btnRecreatePaintingID = view.findViewById<MaterialButton>(R.id.btn_publish_recreate_paintign_id)
+        val btnCopyPaintingID = view.findViewById<MaterialButton>(R.id.btn_publish_copy_painting_id)
 
         paintingID = generatePaintingID()
 
@@ -64,6 +75,30 @@ class PublishPaintingFragment : Fragment() {
                 tvPaintingID.visibility = View.GONE
             } else {
                 tvPaintingID.visibility = View.VISIBLE
+            }
+        }
+
+        btnRecreatePaintingID.setOnClickListener {
+            paintingID = generatePaintingID()
+
+            tvPaintingID.text = paintingID
+        }
+
+        btnCopyPaintingID.setOnClickListener {
+            val activity = activity
+
+            activity?.let {
+                val sdk = Build.VERSION.SDK_INT
+                if (sdk < Build.VERSION_CODES.HONEYCOMB) {
+                    val clipboard: ClipboardManager? = getSystemService(activity, ClipboardManager::class.java)
+                    clipboard?.text = "text to clip"
+                } else {
+                    val clipboard: ClipboardManager? = getSystemService(activity, ClipboardManager::class.java)
+                    val clip = ClipData.newPlainText("paintingID", paintingID)
+                    clipboard?.setPrimaryClip(clip)
+                }
+
+                Snackbar.make(view, "Copied to clipboard", Snackbar.LENGTH_SHORT).show()
             }
         }
 
