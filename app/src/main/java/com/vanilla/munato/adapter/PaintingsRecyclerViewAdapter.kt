8369505ec148e370d7e9642abbbb1fc74df31931
kmortyk <casha.dryaba@mail.ru>
2021-07-23 00:19:28 +0300

    avg color caching

--- app/src/main/java/com/vanilla/munato/adapter/PaintingsRecyclerViewAdapter.kt
+++ app/src/main/java/com/vanilla/munato/adapter/PaintingsRecyclerViewAdapter.kt
@@ -2,12 +2,12 @@ package com.vanilla.munato.adapter
 
 import android.content.res.ColorStateList
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,13 +17,13 @@ import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import com.vanilla.munato.R
 import com.vanilla.munato.activity.HomeActivity
 import com.vanilla.munato.model.PaintingDownloadData
+import com.vanilla.munato.model.PaintingViewData
 
 
 const val PAINTINGS_DIFFERENCE = true
@@ -34,67 +34,31 @@ class PaintingCardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
     private val tvStarsCount: TextView = itemView.findViewById(R.id.itm_stars_text)
     private val cardViewAccent: CardView = itemView.findViewById(R.id.accent_card)
 
-    fun setData(painting: PaintingDownloadData) {
+    fun setData(painting: PaintingViewData) {
 //        tvHeader.text = painting.model.name
-        tvStarsCount.text = painting.model.stars.toString()
+        tvStarsCount.text = painting.getModel().stars.toString()
 
         // start loading preview
-        if(painting.previewURL != Uri.EMPTY) {
+        if(painting.getPreviewURL() != Uri.EMPTY) {
             Glide.with(itemView.context)
-                .load(painting.previewURL)
+                .load(painting.getPreviewURL())
                 .listener(object : RequestListener<Drawable> {
                     override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean) = false
 
                     override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
-                        setAverageColorAsAccentCardBackground(resource)
+                        if(resource != null && resource is BitmapDrawable && resource.bitmap != null) {
+                            val color = averageColor(resource.bitmap)
+                            cardViewAccent.backgroundTintList = ColorStateList.valueOf(color)
+                            painting.averageColor = color // cache average to model
+                        }
+
                         return false
                     }
                 })
                 .into(ivCardCover)
         } else {
-            setAverageColorAsAccentCardBackground(ivCardCover.drawable)
+            cardViewAccent.backgroundTintList = ColorStateList.valueOf(painting.averageColor)
         }
-    }
-
-    private fun setAverageColorAsAccentCardBackground(drawable: Drawable?) {
-        val bitmap = drawableToBitmap(drawable)
-        bitmap?.let {
-            val color = averageColor(bitmap)
-            cardViewAccent.backgroundTintList = ColorStateList.valueOf(color)
-        }
-    }
-
-    // https://stackoverflow.com/questions/3035692/how-to-convert-a-drawable-to-a-bitmap
-
-    private fun drawableToBitmap(drawable: Drawable?): Bitmap? {
-        if(drawable == null) {
-            return null
-        }
-
-        var bitmap: Bitmap? = null
-        if (drawable is BitmapDrawable) {
-            val bitmapDrawable = drawable
-            if (bitmapDrawable.bitmap != null) {
-                return bitmapDrawable.bitmap
-            }
-        }
-        bitmap = if (drawable.intrinsicWidth <= 0 || drawable.intrinsicHeight <= 0) {
-            Bitmap.createBitmap(
-                1,
-                1,
-                Bitmap.Config.ARGB_8888
-            ) // Single color bitmap will be created of 1x1 pixel
-        } else {
-            Bitmap.createBitmap(
-                drawable.intrinsicWidth,
-                drawable.intrinsicHeight,
-                Bitmap.Config.ARGB_8888
-            )
-        }
-        val canvas = Canvas(bitmap)
-        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight())
-        drawable.draw(canvas)
-        return bitmap
     }
 
     // https://stackoverflow.com/questions/12408431/how-can-i-get-the-average-colour-of-an-image
@@ -127,7 +91,7 @@ class PaintingCardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 class PaintingsRecyclerViewAdapter(private val activity: HomeActivity) :
     RecyclerView.Adapter<PaintingCardViewHolder>() {
 
-    private val paintings: MutableList<PaintingDownloadData> = mutableListOf()
+    private val paintings: MutableList<PaintingViewData> = mutableListOf()
 
     companion object {
         const val TYPE_BIG = 1
@@ -147,14 +111,14 @@ class PaintingsRecyclerViewAdapter(private val activity: HomeActivity) :
     override fun onBindViewHolder(holder: PaintingCardViewHolder, position: Int) {
         holder.setData(paintings[position])
         holder.itemView.setOnClickListener {
-            activity.openPaintingViewFragment(paintings[position])
+            activity.openPaintingViewFragment(paintings[position].paintingDownloadData)
         }
     }
 
     override fun getItemCount() = paintings.size
 
     override fun getItemViewType(position: Int): Int {
-        return if(paintings[position].model.stars > 10) {
+        return if(paintings[position].getModel().stars > 10) {
             TYPE_BIG
         } else {
             TYPE_SMALL
@@ -162,7 +126,7 @@ class PaintingsRecyclerViewAdapter(private val activity: HomeActivity) :
     }
 
     fun addData(paintingDownloadData: PaintingDownloadData) {
-        paintings.add(paintingDownloadData)
+        paintings.add(PaintingViewData(paintingDownloadData))
         notifyItemInserted(paintings.size - 1)
     }
 }
