91ca53c931160f5547ba2c1b4462eeebd75aadcc
kmortyk <casha.dryaba@mail.ru>
2021-07-23 00:26:40 +0300

    disk cache strategy all

--- app/src/main/java/com/vanilla/munato/adapter/PaintingsRecyclerViewAdapter.kt
+++ app/src/main/java/com/vanilla/munato/adapter/PaintingsRecyclerViewAdapter.kt
@@ -17,6 +17,7 @@ import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
@@ -42,6 +43,7 @@ class PaintingCardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
         if(painting.getPreviewURL() != Uri.EMPTY) {
             Glide.with(itemView.context)
                 .load(painting.getPreviewURL())
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .listener(object : RequestListener<Drawable> {
                     override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean) = false
 
