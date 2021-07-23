7b3d6cc58078a7691b0d6b01693e07bd19e08baf
kmortyk <casha.dryaba@mail.ru>
2021-07-24 01:16:07 +0300

    rem var

--- app/src/main/java/com/vanilla/munato/model/PaintingPublishData.kt
+++ app/src/main/java/com/vanilla/munato/model/PaintingPublishData.kt
@@ -1,9 +1,5 @@
 package com.vanilla.munato.model
 
-import android.graphics.Bitmap
-
-val EmptyPaintingPreview = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888)
-
 data class PaintingPublishData(
     val model: PaintingModel,
     val preview: PaintingPreview
