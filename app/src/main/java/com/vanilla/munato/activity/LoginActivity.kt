11b1b00d13e45507c1293f34c66d4d8d28fcb668
Eldar Kurbanov <EldarK@outlook.com>
2021-07-28 00:18:38 +0300

    Revert to main branch

--- app/src/main/java/com/vanilla/munato/activity/LoginActivity.kt
+++ app/src/main/java/com/vanilla/munato/activity/LoginActivity.kt
@@ -5,8 +5,6 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.Toast
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
@@ -26,8 +24,6 @@ import java.lang.NullPointerException
 *  */
 
 class LoginActivity : AppCompatActivity() {
-
-    val LOG_TAG = "LoginActivity"
 
     // See: https://developer.android.com/training/basics/intents/result
     private val signInLauncher = registerForActivityResult(
@@ -80,13 +76,8 @@ class LoginActivity : AppCompatActivity() {
 
         val user = FirebaseAuth.getInstance().currentUser
         if (user == null) {
-            hideUI()
             runFirebaseLogin()
-
-            return
         }
-
-        showUI()
     }
 
     private fun runFirebaseLogin() {
@@ -95,22 +86,18 @@ class LoginActivity : AppCompatActivity() {
 
     private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
         val response = result.idpResponse
-        if (result.resultCode != RESULT_OK) {
-            Log.e(LOG_TAG, "onSignInResult result code: " + result.resultCode.toString())
-
-            val errorCode = response?.error?.errorCode
-            errorCode?.let {
-                Snackbar.make(binding.root, "${resources.getString(R.string.login_err_code)} $it", Snackbar.LENGTH_LONG).show()
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+            if (user != null) {
+                return
             }
         }
-    }
 
-    private fun hideUI() {
-        binding.loginUi.visibility = View.INVISIBLE
-    }
-
-    private fun showUI() {
-        binding.loginUi.visibility = View.VISIBLE
+        val errorCode = response?.error?.errorCode
+        errorCode?.let {
+            Snackbar.make(binding.root, "${resources.getString(R.string.login_err_code)} $it", Snackbar.LENGTH_LONG).show()
+        }
     }
 }
 
